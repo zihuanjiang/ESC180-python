f133692875ff77ca4d71e9c5416f7f837388c7ac
Ken Jiang <94940723+zihuanjiang@users.noreply.github.com>
2023-06-04 14:36:42 -0400

    Add files via upload

---
+++ lab 1.py
@@ -0,0 +1,23 @@
+#Problem one
+print("Hello, Python")
+
+#Problem two
+print("Hello, Zihuan Jiang") #
+print("Hello, Yuyang Zhang")
+
+#Probelm three
+Name_A = "Zihuan Jiang"
+Name_B = "Yuyang Zhang"
+print("Hello, " + Name_A + " and " + Name_B + ". Your names are " + Name_A + " and " +  Name_B + ". Hi there. Your names are still " + Name_A + " and " + Name_B + ".")
+
+#Problem four
+Name_A = "Prof. Cluett"
+Name_B = "Prof. Thywissen"
+
+#Problem five
+greetee = input("What is your name: ")
+
+if greetee == "Lord Voldemort":
+    print("I'm not talking to you.")
+else:
+    print("Hello, " + greetee +  ".")
